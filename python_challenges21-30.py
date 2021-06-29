ee1da2d58b9f42f29bae3a70acb0aac8afe36221
Dan P <73758508+benochi@users.noreply.github.com>
2021-06-29 11:59:42 -0500

    Update python_challenges21-30.py

--- python_challenges21-30.py
+++ python_challenges21-30.py
@@ -2,6 +2,12 @@ def triple_and_filter(nums):
     """Return new list of tripled nums for those nums divisible by 4.
 Return every number in list that is divisible by 4 in a new list,
  except multipled by 3. >>> triple_and_filter([1, 2, 3, 4]) [12] >>> triple_and_filter([6, 8, 10, 12]) [24, 36] >>> triple_and_filter([1, 2]) [] """
+    new_list = []
+    for i in nums:
+        if i % 4 == 0:
+            new_list.append(i * 3)
+    print(new_list)
+        
     
 def extract_full_names(people):
     """Return list of names, extracting from first+last keys in people dicts.
@@ -9,7 +15,9 @@ def extract_full_names(people):
   Returns list of space-separated first and last names.
  >>> names = [ ...     {'first': 'Ada', 'last': 'Lovelace'}, ...     {'first': 'Grace', 'last': 'Hopper'}, ... ]
  >>> extract_full_names(names) ['Ada Lovelace', 'Grace Hopper'] """
-    
+    return f"{name['first']} {name['last']}" for name in people
+
+
 def sum_floats(nums):
     """Return sum of floating point numbers in nums.
 >>> sum_floats([1.5, 2.4, 'awesome', [], 1]) 3.9 >>> sum_floats([1, 2, 3]) 0 """
@@ -17,33 +25,64 @@ def sum_floats(nums):
     # hint: to find out if something is a float, you should use the
     # "isinstance" function --- research how to use this to find out
     # if something is a float!
+    sum = 0
+    for num in nums:
+        if isinstance(num, float):
+            sum += num
+    print(sum)
 
 def list_check(lst):
-    """Are all items in lst a list?
- >>> list_check([[1], [2, 3]]) True >>> list_check([[1], "nope"]) False
- """
+    """Are all items in lst a list? >>> list_check([[1], [2, 3]]) True >>> list_check([[1], "nope"]) False"""
+    for item in lst:
+        if not isinstance(item, list):
+            return False
+
+    return True
+    
 def remove_every_other(lst):
     """Return a new list of other item.
  >>> lst = [1, 2, 3, 4, 5] >>> remove_every_other(lst) [1, 3, 5] This should return a list, not mutate the original: >>> lst [1, 2, 3, 4, 5] """
-    
+    return lst[::2]
+
 def sum_pairs(nums, goal):
     """Return tuple of first pair of nums that sum to goal.
  For example: >>> sum_pairs([1, 2, 2, 10], 4) (2, 2) (4, 2) sum to 6, and come before (5, 1): >>> sum_pairs([4, 2, 10, 5, 1], 6) # (4, 2) (4, 2)(4, 3) 
  sum to 7, and finish before (5, 2):  >>> sum_pairs([5, 1, 4, 8, 3, 2], 7) (4, 3) No pairs sum to 100, so return empty tuple:>>>
  sum_pairs([11, 20, 4, 2, 1, 5], 100) () """
+    new_set = set()
+    for i in nums:
+        remainder = goal - i
+        if remainder in new_set:
+            return (remainder, i)
+        new_set.add(i)
+
+    return ()
     
 def vowel_count(phrase):
     """Return frequency map of vowels, case-insensitive.
  >>> vowel_count('rithm school') {'i': 1, 'o': 2} >>> vowel_count('HOW ARE YOU? i am great!') 
  {'o': 2, 'a': 3, 'e': 2, 'u': 1, 'i': 1} """
+    #*maybe* print(*map(phrase.lower().count, "aeiou"))
+    phrase = phrase.lower()
+    counter = {}
+    vowels = 'aeiou'
+    
+    for char in phrase:
+        if char in vowels:
+            counter[char] = counter.get(char, 0) + 1
+    return counter
     
 def titleize(phrase):
     """Return phrase in title case (each word capitalized). >>> titleize('this is awesome') 'This Is Awesome'
  >>> titleize('oNLy cAPITALIZe fIRSt') 'Only Capitalize First' """
-    
+    return phrase.title()
+
 def find_factors(num):
     """Find factors of num, in increasing order. >>> find_factors(10) [1, 2, 5, 10] >>> find_factors(11) [1, 11]
 >>> find_factors(111) [1, 3, 37, 111]>>> find_factors(321421) [1, 293, 1097, 321421] """
+    for i in range(1, num + 1):
+        if num % i == 0:
+            print(i)
     
 def includes(collection, sought, start=None):
     """Is sought in collection, starting at index start?
@@ -56,10 +95,20 @@ def includes(collection, sought, start=None):
  >>> includes([1, 2, 3], 1) True >>> includes([1, 2, 3], 1, 2) False >>> includes("hello", "o") True >>> includes(('Elmo', 5, 'red'), 'red', 1) True
  >>> includes({1, 2, 3}, 1) True >>> includes({1, 2, 3}, 1, 3)  # "start" ignored for sets! True >>> includes({"apple": "red", "berry": "blue"}, "blue")
  True """
+    if isinstance(collection, dict):
+        return sought in collection.values()
+    if start is None or isinstance(collection, set):
+        return sought in collection
+    return sought in collection[start:]
     
-    def repeat(phrase, num):
+    
+def repeat(phrase, num):        
     """Return phrase, repeated num times.
  >>> repeat('*', 3)  '***' >>> repeat('abc', 2) 'abcabc' >>> repeat('abc', 0) '' Ignore illegal values of num and return None:
  >>> repeat('abc', -1) is None True
  >>> repeat('abc', 'nope') is None
  True """
+    if not isinstance(num, int) or num < 0:
+        return None
+
+    return phrase * num
