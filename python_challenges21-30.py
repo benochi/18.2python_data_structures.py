d949688695e0ec9addfb85197e9d7359adf5c2b0
Dan P <73758508+benochi@users.noreply.github.com>
2021-06-29 10:22:34 -0500

    Update python_challenges21-30.py

--- python_challenges21-30.py
+++ python_challenges21-30.py
@@ -1,45 +1,18 @@
 def triple_and_filter(nums):
     """Return new list of tripled nums for those nums divisible by 4.
-
-    Return every number in list that is divisible by 4 in a new list,
-    except multipled by 3.
-    
-        >>> triple_and_filter([1, 2, 3, 4])
-        [12]
-        
-        >>> triple_and_filter([6, 8, 10, 12])
-        [24, 36]
-        
-        >>> triple_and_filter([1, 2])
-        []
-    """
+Return every number in list that is divisible by 4 in a new list,
+ except multipled by 3. >>> triple_and_filter([1, 2, 3, 4]) [12] >>> triple_and_filter([6, 8, 10, 12]) [24, 36] >>> triple_and_filter([1, 2]) [] """
     
 def extract_full_names(people):
     """Return list of names, extracting from first+last keys in people dicts.
-
-    - people: list of dictionaries, each with 'first' and 'last' keys for
-              first and last names
-
-    Returns list of space-separated first and last names.
-
-        >>> names = [
-        ...     {'first': 'Ada', 'last': 'Lovelace'},
-        ...     {'first': 'Grace', 'last': 'Hopper'},
-        ... ]
-
-        >>> extract_full_names(names)
-        ['Ada Lovelace', 'Grace Hopper']
-    """
+ - people: list of dictionaries, each with 'first' and 'last' keys for first and last names
+  Returns list of space-separated first and last names.
+ >>> names = [ ...     {'first': 'Ada', 'last': 'Lovelace'}, ...     {'first': 'Grace', 'last': 'Hopper'}, ... ]
+ >>> extract_full_names(names) ['Ada Lovelace', 'Grace Hopper'] """
     
 def sum_floats(nums):
     """Return sum of floating point numbers in nums.
-    
-        >>> sum_floats([1.5, 2.4, 'awesome', [], 1])
-        3.9
-        
-        >>> sum_floats([1, 2, 3])
-        0
-    """
+>>> sum_floats([1.5, 2.4, 'awesome', [], 1]) 3.9 >>> sum_floats([1, 2, 3]) 0 """
 
     # hint: to find out if something is a float, you should use the
     # "isinstance" function --- research how to use this to find out
@@ -47,131 +20,46 @@ def sum_floats(nums):
 
 def list_check(lst):
     """Are all items in lst a list?
-
-        >>> list_check([[1], [2, 3]])
-        True
-
-        >>> list_check([[1], "nope"])
-        False
-    """
+ >>> list_check([[1], [2, 3]]) True >>> list_check([[1], "nope"]) False
+ """
 def remove_every_other(lst):
     """Return a new list of other item.
-
-        >>> lst = [1, 2, 3, 4, 5]
-
-        >>> remove_every_other(lst)
-        [1, 3, 5]
-
-    This should return a list, not mutate the original:
-
-        >>> lst
-        [1, 2, 3, 4, 5]
-    """
+ >>> lst = [1, 2, 3, 4, 5] >>> remove_every_other(lst) [1, 3, 5] This should return a list, not mutate the original: >>> lst [1, 2, 3, 4, 5] """
+    
 def sum_pairs(nums, goal):
     """Return tuple of first pair of nums that sum to goal.
-
-    For example:
-
-        >>> sum_pairs([1, 2, 2, 10], 4)
-        (2, 2)
-
-    (4, 2) sum to 6, and come before (5, 1):
-
-        >>> sum_pairs([4, 2, 10, 5, 1], 6) # (4, 2)
-        (4, 2)
-
-    (4, 3) sum to 7, and finish before (5, 2):
-
-        >>> sum_pairs([5, 1, 4, 8, 3, 2], 7)
-        (4, 3)
-
-    No pairs sum to 100, so return empty tuple:
-
-        >>> sum_pairs([11, 20, 4, 2, 1, 5], 100)
-        ()
-    """
+ For example: >>> sum_pairs([1, 2, 2, 10], 4) (2, 2) (4, 2) sum to 6, and come before (5, 1): >>> sum_pairs([4, 2, 10, 5, 1], 6) # (4, 2) (4, 2)(4, 3) 
+ sum to 7, and finish before (5, 2):  >>> sum_pairs([5, 1, 4, 8, 3, 2], 7) (4, 3) No pairs sum to 100, so return empty tuple:>>>
+ sum_pairs([11, 20, 4, 2, 1, 5], 100) () """
+    
 def vowel_count(phrase):
     """Return frequency map of vowels, case-insensitive.
-
-        >>> vowel_count('rithm school')
-        {'i': 1, 'o': 2}
-        
-        >>> vowel_count('HOW ARE YOU? i am great!') 
-        {'o': 2, 'a': 3, 'e': 2, 'u': 1, 'i': 1}
-    """
+ >>> vowel_count('rithm school') {'i': 1, 'o': 2} >>> vowel_count('HOW ARE YOU? i am great!') 
+ {'o': 2, 'a': 3, 'e': 2, 'u': 1, 'i': 1} """
+    
 def titleize(phrase):
-    """Return phrase in title case (each word capitalized).
-
-        >>> titleize('this is awesome')
-        'This Is Awesome'
-
-        >>> titleize('oNLy cAPITALIZe fIRSt')
-        'Only Capitalize First'
-    """
+    """Return phrase in title case (each word capitalized). >>> titleize('this is awesome') 'This Is Awesome'
+ >>> titleize('oNLy cAPITALIZe fIRSt') 'Only Capitalize First' """
+    
 def find_factors(num):
-    """Find factors of num, in increasing order.
-
-    >>> find_factors(10)
-    [1, 2, 5, 10]
-
-    >>> find_factors(11)
-    [1, 11]
-
-    >>> find_factors(111)
-    [1, 3, 37, 111]
-
-    >>> find_factors(321421)
-    [1, 293, 1097, 321421]
-    """
+    """Find factors of num, in increasing order. >>> find_factors(10) [1, 2, 5, 10] >>> find_factors(11) [1, 11]
+>>> find_factors(111) [1, 3, 37, 111]>>> find_factors(321421) [1, 293, 1097, 321421] """
+    
 def includes(collection, sought, start=None):
     """Is sought in collection, starting at index start?
-
     Return True/False if sought is in the given collection:
     - lists/strings/sets/tuples: returns True/False if sought present
     - dictionaries: return True/False if *value* of sought in dictionary
-
     If string/list/tuple and `start` is provided, starts searching only at that
     index. This `start` is ignored for sets/dictionaries, since they aren't
     ordered.
-
-        >>> includes([1, 2, 3], 1)
-        True
-
-        >>> includes([1, 2, 3], 1, 2)
-        False
-
-        >>> includes("hello", "o")
-        True
-
-        >>> includes(('Elmo', 5, 'red'), 'red', 1)
-        True
-
-        >>> includes({1, 2, 3}, 1)
-        True
-
-        >>> includes({1, 2, 3}, 1, 3)  # "start" ignored for sets!
-        True
-
-        >>> includes({"apple": "red", "berry": "blue"}, "blue")
-        True
-    """
+ >>> includes([1, 2, 3], 1) True >>> includes([1, 2, 3], 1, 2) False >>> includes("hello", "o") True >>> includes(('Elmo', 5, 'red'), 'red', 1) True
+ >>> includes({1, 2, 3}, 1) True >>> includes({1, 2, 3}, 1, 3)  # "start" ignored for sets! True >>> includes({"apple": "red", "berry": "blue"}, "blue")
+ True """
+    
     def repeat(phrase, num):
     """Return phrase, repeated num times.
-
-        >>> repeat('*', 3)
-        '***'
-
-        >>> repeat('abc', 2)
-        'abcabc'
-
-        >>> repeat('abc', 0)
-        ''
-
-    Ignore illegal values of num and return None:
-
-        >>> repeat('abc', -1) is None
-        True
-
-        >>> repeat('abc', 'nope') is None
-        True
-    """
+ >>> repeat('*', 3)  '***' >>> repeat('abc', 2) 'abcabc' >>> repeat('abc', 0) '' Ignore illegal values of num and return None:
+ >>> repeat('abc', -1) is None True
+ >>> repeat('abc', 'nope') is None
+ True """
